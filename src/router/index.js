a52b184167a0a1f7664ec7bfc7689ba0aaad25a8
AlaveC <kloudw1ld@gmail.com>
2025-08-14 15:39:55 -0500

    actualizado

--- src/router/index.js
+++ src/router/index.js
@@ -12,80 +12,12 @@ const routes = [
         name: "inicio",
         component: Inicio,
       },
+      
+  
       {
-        path: "noticias",
-        name: "noticias",
-        component: () => import("@/components/page/noticias/noticiasPadre.vue"),
-      },
-      {
-        path: "noticias/:id",
-        name: "noticiaCompleta",
-        component: () => import("@/components/page/noticias/noticiaCompleta.vue"),
-      },
-      {
-        path: "comunicados",
-        name: "comunicados",
-        component: () => import("@/components/page/comunicados/comunicadosPadre.vue"),
-      },
-      {
-        path: "comunicados/:id",
-        name: "comunicadoCompleto",
-        component: () => import("@/components/page/comunicados/comunicadoCompleto.vue"),
-      },
-      {
-        path: "videos",
-        name: "videos",
-        component: () => import("@/components/page/videos.vue"),
-      },
-      {
-        path: "servicios",
-        name: "servicios",
-        component: () => import("@/components/common/services.vue"),
-      },
-      {
-        path: "gestion-institucional",
-        name: "gestion-institucional",
-        component: () => import("@/components/page/gestionInstitucional.vue"),
-      },
-      {
-        path: "gestion-pedagogica",
-        name: "gestion-pedagogica",
-        component: () => import("@/components/page/GestionPedagogica.vue"),
-      },
-      {
-        path: "gestion-administrativa",
-        name: "gestion-administrativa",
-        component: () => import("@/components/page/GestionAdministrativa.vue"),
-      },
-      {
-        path: "asesoria-legal",
-        name: "asesoria-legal",
-        component: () => import("@/components/page/asesoriaLegal.vue"),
-      },
-      {
-        path: "control-interno",
-        name: "control-interno",
-        component: () => import("@/components/page/controlInterno.vue"),
-      },
-      {
-        path: "directorio",
-        name: "directorio",
-        component: () => import("@/components/page/directorio.vue"),
-      },
-      {
-        path: "contacto",
-        name: "contacto",
-        component: () => import("@/components/page/contacto.vue"),
-      },
-      {
-        path: "portalTransparencia",
-        name: "portalTransparencia",
-        component: () => import("@/components/page/portalTransparencia.vue"),
-      },
-
-      {
-        path: "administracion-marketing",
-        name: "administracion-marketing",
+        path: "programas/administracion",
+        name: "administracion",
+        // Asegúrate de que este componente exista o créalo
         component: () => import("@/components/page/carreras/administracionMarketing.vue"),
       },
     ],
@@ -100,6 +32,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  // Esto hace que la página suba al principio cada vez que cambias de ruta
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
